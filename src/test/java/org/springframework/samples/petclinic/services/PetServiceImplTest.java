8bcd44609c274959894741956a0cc0f13907faf1
lucmarbay <lukimartinezbayon@gmail.com>
2019-10-17 11:14:14 +0200

    Creacion del test de PetService

---
+++ src/test/java/org/springframework/samples/petclinic/services/PetServiceImplTest.java
@@ -0,0 +1,39 @@
+package org.springframework.samples.petclinic.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PetServiceImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdatePet() {
+		fail("Not yet implemented");
+	}
+
+}
