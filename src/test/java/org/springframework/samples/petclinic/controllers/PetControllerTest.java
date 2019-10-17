7d5253ac019c96188bdc16988062d81aad852b8c
lucmarbay <lukimartinezbayon@gmail.com>
2019-10-17 16:57:27 +0200

    Desarrollando el MockMvc

---
+++ src/test/java/org/springframework/samples/petclinic/controllers/PetControllerTest.java
@@ -0,0 +1,39 @@
+package org.springframework.samples.petclinic.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PetControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetAllPets() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateOrSavePet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdatePet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeletePet() {
+		fail("Not yet implemented");
+	}
+
+}
