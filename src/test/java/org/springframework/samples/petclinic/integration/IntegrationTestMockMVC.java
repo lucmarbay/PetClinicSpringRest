b12a42c188e69f05705f292dc90894c041201926
lucmarbay <lukimartinezbayon@gmail.com>
2019-10-22 15:31:05 +0200

    terminado los test de IntegrationTestMKockMVC

--- src/test/java/org/springframework/samples/petclinic/integration/IntegrationTestMockMVC.java
+++ src/test/java/org/springframework/samples/petclinic/integration/IntegrationTestMockMVC.java
@@ -1,7 +1,9 @@
 package org.springframework.samples.petclinic.integration;
 
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -14,14 +16,19 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.samples.petclinic.PetClinicApplication;
+import org.springframework.samples.petclinic.dto.PetDTO;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import junit.framework.Assert;
 
@@ -32,6 +39,9 @@ public class IntegrationTestMockMVC {
 
 	@Autowired
 	private WebApplicationContext wac;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
 	
 	private MockMvc mockMvc;
 	@Before
@@ -50,11 +60,21 @@ public class IntegrationTestMockMVC {
 	}
 	
 	@Test
-	public void findAll() throws Exception {
+	public void findAllTest() throws Exception {
 		this.mockMvc.perform(get("/pet/"))
 	            .andExpect(status().isOk())
 				.andExpect(jsonPath("$[0].name", is("Leo")))
 				.andExpect(jsonPath("$[12].name", is("Sly")));
-	    
+	}
+	@Test
+	public void savePetTest() throws Exception {
+		PetDTO pet = new PetDTO();
+		pet.setName("Alex");
+		
+		this.mockMvc.perform(post("/pet/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(pet)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name", is("Alex")));
 	}
 }
